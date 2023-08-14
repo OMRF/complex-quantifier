6021e83cc6e8ec0473e05acc0322334e86102820
Eric Wang <eric.wang401@gmail.com>
2023-08-14 16:06:11 +0000

    add tailwind + other necessary dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	// https://github.com/r-wasm/webr/issues/203
+</script>
+<input id="fileSelect" type="file" accept=".csv" />
