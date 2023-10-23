39428e226bcff350b4803402f4ce9b3bc7e2e139
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-10-23 15:10:11 -0500

    update header

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -1,16 +1,29 @@
 <script>
-    import Logo from '$lib/assets/icons/Logo.svelte'
     import ContentContainer from '$lib/components/ContentContainer.svelte'
+    import Spinner from '$lib/components/Spinner.svelte'
+    import { getVersion } from '@tauri-apps/api/app'
 </script>
 
-<div class='w-full h-2 bg-gradient-to-r from-[#2AF598] to-[#009EFD]' />
+<div class="w-full h-2 bg-gradient-to-r from-[#2AF598] to-[#009EFD]" />
 
 <ContentContainer>
-    <div class="space-y-1.5 pt-8 lg:pt-12 pb-16">
-        <h1 class="font-heading font-semibold text-2xl md:text-5xl lg:text-7xl">Complex Quantifier</h1>
+    <div class="space-y-1.5 pb-6 pt-8 lg:pt-12">
+        <h1 class="font-heading text-2xl font-semibold md:text-5xl lg:text-7xl">
+            Complex Quantifier {#await getVersion()}
+                <Spinner class="inline h-4 w-4" />
+            {:then version}
+                <span class="text-sm font-normal text-slate-500">v{version}</span>
+            {/await}
+        </h1>
         <p class="text-muted-foreground max-w-2xl">
             A utility for calculating geometric means from a spreadsheet, picking them into separate worksheets, and
-            returning the calculations as an Excel workbook.
+            returning the calculations as an Excel workbook. Created by <a
+                class="font-medium underline underline-offset-4"
+                href="https://github.com/ericwang401"
+                target="_blank"
+            >
+                Eric Wang
+            </a>
         </p>
     </div>
 
